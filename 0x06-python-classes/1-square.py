0dbf0d188d356f7365fe9b91bd67c4c54fb145b4
eagleae7 <spotarinz@gmail.com>
2023-01-24 16:09:55 +0100

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Class Square"""
+"""
+Class Square:=> Create a square class that contains a size
+"""
 
 
-class Square: 
+class Square:
+    """Class that defines a square with size"""
+
     def __init__(self, size):
         """Initialize variables"""
         self.__size = size
