4d2752be79864bd8aa9b336c779fd58e04468182
eagleae7 <spotarinz@gmail.com>
2023-02-01 12:29:34 +0100

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle. 
+"""Solves the N-queens puzzle.
 Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
 where 'r' and 'c' represent the row and column, respectively, where a
 queen must be placed on the chessboard.
