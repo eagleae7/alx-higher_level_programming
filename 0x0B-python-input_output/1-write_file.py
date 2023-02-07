88312866eebdbd4c5322dd100c3b75f5a2544f8f
eagleae7 <spotarinz@gmail.com>
2023-02-07 12:50:33 +0100

    py_inheritance

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Defines a file-writing function."""
+"""Program that writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
+    """function that writes a string to a text file (UTF8)
+    and returns the number of characters written
     """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
